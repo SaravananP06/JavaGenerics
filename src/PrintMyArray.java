fcd868039d672ed422d43b63b6c7814b368f584d
SaravananP06 <saravanan.palaniswami6@gmail.com>
2023-05-01 23:17:13 +0530

    Printing with different datatypes

--- src/PrintMyArray.java
+++ src/PrintMyArray.java
@@ -1,5 +1,29 @@
 public class PrintMyArray {
+
+    public static void toPrint(Integer[] inputArray){
+        for (int element: inputArray) {
+            System.out.println(element);
+        }
+    }
+
+    public static void toPrint(Double[] inputArray){
+        for (double element: inputArray) {
+            System.out.println(element);
+        }
+    }
+
+    public static void toPrint(Character[] inputArray){
+        for (char element: inputArray) {
+            System.out.println(element);
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("This is Java Generics");
+        Integer[] intArray = {1,2,3,4};
+        Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
+        Character[] charArray = {'a','b','c','d'};
+
+        toPrint(intArray);
+        toPrint(doubleArray);
+        toPrint(charArray);
     }
 }
