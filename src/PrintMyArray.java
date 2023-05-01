cc65724d287aa221d6c330c446e48209ac969d65
SaravananP06 <saravanan.palaniswami6@gmail.com>
2023-05-01 23:25:02 +0530

    printing arrays using generics

--- src/PrintMyArray.java
+++ src/PrintMyArray.java
@@ -1,22 +1,12 @@
 public class PrintMyArray {
 
-    public static void toPrint(Integer[] inputArray){
-        for (int element: inputArray) {
+    public static <E> void toPrint(E[] inputArray){
+        for (E element: inputArray) {
             System.out.println(element);
         }
+        System.out.println();
     }
 
-    public static void toPrint(Double[] inputArray){
-        for (double element: inputArray) {
-            System.out.println(element);
-        }
-    }
-
-    public static void toPrint(Character[] inputArray){
-        for (char element: inputArray) {
-            System.out.println(element);
-        }
-    }
     public static void main(String[] args) {
         Integer[] intArray = {1,2,3,4};
         Double[] doubleArray = {1.1, 2.2, 3.3, 4.4};
